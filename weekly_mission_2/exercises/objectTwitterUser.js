d22e8eef0e7a26cb5ada9c4cbd8acbc5a1909bd9
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-15 01:17:00 -0500

    ejercicio 1.3 terminado

---
+++ weekly_mission_2/exercises/objectTwitterUser.js
@@ -0,0 +1,18 @@
+const User = {
+    name : "Andrés Santiago",
+    userName : "A_Santiago_5",
+    joinedIn : "April 2013",
+    following : 14,
+    followers : 10,
+    numberTweets : 873,
+    getStatus: function(){
+        return `The user ${this.userName} has got ${this.followers} followers since ${this.joinedIn}`
+    },
+    getNameAndTweets: function(){
+        return `The user which name is ${this.name} has published ${this.numberTweets} tweets`
+    }
+}
+
+console.log(User);
+console.log(User.getStatus());
+console.log(User.getNameAndTweets());
