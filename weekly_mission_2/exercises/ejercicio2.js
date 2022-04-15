8740524d0834347de2709a7f6d81584d576f4e9b
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-15 14:30:37 -0500

    Ejercicio 2.7 terminado

--- weekly_mission_2/exercises/ejercicio2.js
+++ weekly_mission_2/exercises/ejercicio2.js
@@ -60,18 +60,26 @@ const explorers = [
    ]
 
 // Ejercicio 2.1
+console.log("2.1 - Nombre de cada explorer")
 explorers.forEach(elementOfArray => console.log(elementOfArray.name))
 // Ejercicio 2.2
+console.log("2.2 - Stack de cada explorer")
 explorers.forEach(elementOfArray => console.log(elementOfArray.stack))
 // Ejercicio 2.3
+console.log("2.3 - Crea una nueva lista con las listas de stacks de cada explorer")
 const NewList = explorers.map(elementOfArray => {return elementOfArray.stack})
 console.log(NewList)
 // Ejercicio 2.4
+console.log("2.4 - Lista de explorers que tengan en su stack 'js'")
 const ListExplorersJS = explorers.filter(explorer => explorer.stack.includes('js'))
 console.log(ListExplorersJS)
 // Ejercicio 2.5 
+console.log("2.5 - Primer explorer que sea de la CDMX")
 console.log(explorers.find(elementOfArray => elementOfArray.city === "CDMX"));
 // Ejercicio 2.6
+console.log("2.6 - Suma de todos los exercises_completed")
 console.log(explorers.reduce((count, elementOfArray) => count + elementOfArray.exercises_completed, 0))
 // Ejercicio 2.7
-console.log()
+console.log("2.7 - Validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true")
+const ExplorerFinishedFrontEnd = explorers.some(element => element.missions.frontend.exercisesFinished === true)
+console.log(ExplorerFinishedFrontEnd)
