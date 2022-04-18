211107578b66e074ded026d094ae19f8fbd9346b
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-18 16:41:03 -0500

    Agragar comentarios y modificación para pasar el test

--- weekly_mission_2/UnitTesting/pokemon.test.js
+++ weekly_mission_2/UnitTesting/pokemon.test.js
@@ -2,5 +2,16 @@
 
 test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')
-  expect(myPokemon.name).toBe('Pikachussss'); // Corrige esta prueba
+  expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
 });
+
+
+// Definocion básica de una prueba
+//test('Test 1) Create a new object pokemon', () => {
+//  expect().toBe();
+//});
+
+// TDD Test Driven Development
+// 1) Escribir una prueba de lo que quieres modelar
+// 2) Correr la prueba y verla fallar
+// 3) Ecribir el código que haga pasar esa prueba
